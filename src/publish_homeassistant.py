b7727e16f7cf7b6bbcaace472e12a8fad0cbdd75
Dregi56 <79653938+Dregi56@users.noreply.github.com>
2026-03-17 09:27:44 +0100

    Delete src directory

--- src/publish_homeassistant.py
+++
@@ -1,9 +0,0 @@
-import paho.mqtt.client as mqtt
-
-def publish_mqtt(broker, topic, value, username=None, password=None):
-    client = mqtt.Client()
-    if username and password:
-        client.username_pw_set(username, password)
-    client.connect(broker, 1883, 60)
-    client.publish(topic, value)
-    client.disconnect()
